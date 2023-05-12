2f9036cea292a8144dbb7f17f02b52b75237d7d7
christopherzulu0 <christopherzulu04@gmail.com>
2023-05-12 21:30:59 +0200

    num fix

--- index.js
+++ index.js
@@ -97,15 +97,14 @@ router.post("/", (req, res) => {
       }
       
     //Count the number of Requests
-    const notification = await User.findOne({ number: phoneNumber });
-const userfind = notification.number;
+   
 
 const userCircles = await Savings.find({
   $or: [
-    { 'GroupMembers.MemberPhoneNumber': userfind },
-    { 'GroupMembers.Creator': userfind },
-    { 'GroupMembers.AdminNumber1': userfind },
-    { 'GroupMembers.AdminNumber2': userfind }
+    { 'GroupMembers.MemberPhoneNumber': phoneNumber },
+    { 'GroupMembers.Creator': phoneNumber },
+    { 'GroupMembers.AdminNumber1': phoneNumber },
+    { 'GroupMembers.AdminNumber2': phoneNumber }
   ]
 });
 
